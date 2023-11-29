474bae072e81e4b89ae92faa21989c103704fe5a
amitbhatnagar <bhatnagaramitcse@gmail.com>
2023-11-29 22:44:25 +0530

    29 nov 2023 confirm screen

--- go4shipuser/lib/confirmRideScreen/confirm_screen.dart
+++ go4shipuser/lib/confirmRideScreen/confirm_screen.dart
@@ -19,7 +19,6 @@ class _ConfirmScreenState extends State<ConfirmScreen> {
 
   @override
   void dispose() {
-
     myController.dispose();
     // TODO: implement dispose
     super.dispose();
@@ -35,9 +34,8 @@ class _ConfirmScreenState extends State<ConfirmScreen> {
       home: WillPopScope(
         onWillPop: pop,
         child: Scaffold(
-        body: Stack(
+            body: Stack(
           children: <Widget>[
-
             GoogleMap(
               myLocationButtonEnabled: false,
               zoomControlsEnabled: false,
@@ -47,7 +45,6 @@ class _ConfirmScreenState extends State<ConfirmScreen> {
                 target: _center,
                 zoom: 14.0,
               ),
-
 
               /*   polylines: {
                 Polyline(
@@ -66,12 +63,123 @@ class _ConfirmScreenState extends State<ConfirmScreen> {
                 if (destination != null) destination
               },
               onLongPress: _addmarker,*/
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              height: 50,
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 8),
+                    width: 10,
+                    height: 10,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.green,
+                    ),
+                  ),
+                  Text(
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      ' Delivery'),
+                  Text(style: TextStyle(fontSize: 16), ''),
+                ],
+              ),
+            ),
+            new Positioned(
+              child: new Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: Container(
+                    height: 155,
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 50,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                  child: Container(
 
+                                margin: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                  color: Colors.orange,
+                                  borderRadius: BorderRadius.circular(5)
+                                ),
+                                height: 25,
+                                child: Center(child: Text('Select Package')),
+                              )),
+                              Expanded(
+                                  child: Container(
+
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                    border: Border.all(color: Colors.orange)),
+                                margin: EdgeInsets.all(5),
+                                height: 25,
+                                child: Center(child: Text('Auto Rickshaw')),
+                              ))
+                            ],
+                          ),
+                        ),
+                        Divider(
+                          height: 1,
+                          color: Colors.grey,
+                        ),
+                        Container(
+                          color: Colors.white,
+                          height: 50,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                  child: Row(
+                                children: [
+                                  Image.asset(
+                                      width: 25,
+                                      height: 25,
+                                      'assets/images/user.png'),
+                                  Text(' Personal'),
+                                ],
+                              )),
+                              Expanded(
+                                  child: Row(
+                                children: [
+                                  Image.asset(
+                                      width: 25,
+                                      height: 25,
+                                      'assets/images/info.png'),
+                                  Text('  Set Payment Method'),
+                                ],
+                              )),
+                            ],
+                          ),
+                        ),
+                        Divider(
+                          height: 1,
+                          color: Colors.grey,
+                        ),
+                        Align(
+                          alignment: FractionalOffset.bottomCenter,
+                          child: Container(
+                            width: double.infinity,
+                            height: 50,
+                            color: Colors.black,
+                            child: Center(
+                                child: Text(
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 14),
+                                    'CONFIRM BOOKING')),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )),
             ),
           ],
-        )
-      ),
-
+        )),
       ),
     );
   }
@@ -82,7 +190,6 @@ class _ConfirmScreenState extends State<ConfirmScreen> {
         position: LatLng(26.7915, 75.2100),
         infoWindow: InfoWindow(
           title: 'My Position',
-        )
-    ),
+        )),
   ];
 }
